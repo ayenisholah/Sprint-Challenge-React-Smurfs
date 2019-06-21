7c7d53e5ec7fffcf0e96f5f723950fb87225b121
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-21 09:39:23 +0100

    individual smurf link added

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import Smurf from './Smurf';
+
 
 class Smurfs extends Component {
   render() {
@@ -10,13 +12,15 @@ class Smurfs extends Component {
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
+              <Link to={`/smurfs/${smurf.id}`} key={smurf.id}>
+                <Smurf
+                  name={smurf.name}
+                  id={smurf.id}
+                  age={smurf.age}
+                  height={smurf.height}
+                  key={smurf.id}
+                />
+              </Link>
             );
           })}
         </ul>
