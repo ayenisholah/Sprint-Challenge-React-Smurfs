9db71a647c1ae9aa42a20f324c7315c45d99c45e
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-21 11:00:09 +0100

    Smurfs component refactored

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 
 import Smurf from './Smurf';
 
@@ -7,19 +7,17 @@ import Smurf from './Smurf';
 class Smurfs extends Component {
   render() {
     return (
-      <div className="Smurfs">
+      <div className="Smurfs-list-wrapper">
         <h1>Smurf Village</h1>
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
-              <Link to={`/smurfs/${smurf.id}`} key={smurf.id}>
-                <Smurf
-                  name={smurf.name}
-                  id={smurf.id}
-                  age={smurf.age}
-                  height={smurf.height}
-                  key={smurf.id}
-                />
+              <Link className="smurf-link" to={`/smurf/${smurf.id}`} key={smurf.id}>
+               <div className="smurf-card">
+                 <p>{smurf.name}</p>
+                 <p>{smurf.age}</p>
+                 <p>{smurf.height}</p>
+               </div>
               </Link>
             );
           })}
